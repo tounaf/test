2dca7e8b3db14b0e7b656b6b6db0d7fc64ebf446
fetra <harinjatovo.fetra@gmail.com>
2018-04-11 15:42:05 +0300

    update

--- config.rb
+++ config.rb
@@ -1,18 +1,19 @@
-Bad Authentication data
+require'twitter'
 client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = " ado3jjwM9PoAtGc8dKIQ8rR7C"
-    config.consumer_secret     = " wSsUexaX7KlmIvTjs0EcSlnUlWUHsucUS4og01tYmn7GBtEoB2"
-    config.access_token        =" 944632994496700418-2gwr4LwBAQbkJ8RN5zT4Mc70a0gNpfJ"
+    config.consumer_key        = "ado3jjwM9PoAtGc8dKIQ8rR7C"
+    config.consumer_secret     = "wSsUexaX7KlmIvTjs0EcSlnUlWUHsucUS4og01tYmn7GBtEoB2"
+    config.access_token        ="944632994496700418-2gwr4LwBAQbkJ8RN5zT4Mc70a0gNpfJ"
     config.access_token_secret ="jWVe50IGnnvgcJ7uL8mHzY0Ewhngzf2cFvEBD9mRNr45M"
-
-client.update("salut word")
-
-
+end
+p client
+client.update("salut word !c'est mon premier tweet via console")
+#client.follow("AndryHajanirina")
+=begin
 def collect_with_max_id(collection=[], max_id=nil, &block)
     response = block.call(max_id)
     collection += response
     response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
-  end
+end
   
   def client.get_all_tweets(user)
     collect_with_max_id do |max_id|
