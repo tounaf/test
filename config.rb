684ce6e3a9e31824ecb123327089560f709e5352
fetra <harinjatovo.fetra@gmail.com>
2018-04-11 14:27:19 +0300

    methode

--- config.rb
+++ config.rb
@@ -8,3 +8,23 @@ client = Twitter::REST::Client.new do |config|
 client.update("salut word")
 
 
+def collect_with_max_id(collection=[], max_id=nil, &block)
+    response = block.call(max_id)
+    collection += response
+    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  end
+  
+  def client.get_all_tweets(user)
+    collect_with_max_id do |max_id|
+      options = {count: 50, include_rts: true}
+      options[:max_id] = max_id unless max_id.nil?
+      user_timeline(user, options)
+    end
+  end
+  
+  recent_tweets = client.get_all_tweets("wjoba")
+
+
+  recent_tweets.each do |tweet|
+	puts tweet.full_text
+end
