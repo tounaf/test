32bd52d229b13225dcf6f0a4e2a4fc9f932935de
fetra <harinjatovo.fetra@gmail.com>
2018-04-11 16:48:03 +0300

    cle

--- config.rb
+++ config.rb
@@ -1,12 +1,14 @@
 require'twitter'
 client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = "ado3jjwM9PoAtGc8dKIQ8rR7C"
-    config.consumer_secret     = "wSsUexaX7KlmIvTjs0EcSlnUlWUHsucUS4og01tYmn7GBtEoB2"
-    config.access_token        ="944632994496700418-2gwr4LwBAQbkJ8RN5zT4Mc70a0gNpfJ"
-    config.access_token_secret ="jWVe50IGnnvgcJ7uL8mHzY0Ewhngzf2cFvEBD9mRNr45M"
+    config.consumer_key          = " "
+    config.consumer_secret       = " "
+    config.access_token          =""
+    config.access_token_secret   =""
 end
 p client
 client.update("salut word !c'est mon premier tweet via console")
+client.follow('NrjMeyer')
+client.follow('tinosam')
 #client.follow("AndryHajanirina")
 =begin
 def collect_with_max_id(collection=[], max_id=nil, &block)
